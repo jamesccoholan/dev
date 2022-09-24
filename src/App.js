d69ff4c0ca11a44e7bf3e89e49babefbd2975380
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-09-24 16:55:45 -0700

    dd

--- src/App.js
+++ src/App.js
@@ -11,10 +11,77 @@ function App() {
     <div>
 
       <header className="App-header">
-        <Home />
+        {/* <Home /> */}
         <div>
+          <form action="/tacos">
+            <p>
+              <label for="username"> Enter a Username</label>
+              <input id="username" type="text" placeholder="username" />
+            </p>
+            <p>
+              <label for="password"> Enter a Password</label>
+              <input type="password" placeholder="password" />
+            </p>
+            <p>
+              <label for="firstName">First Name</label>
+              <input type="name"></input>
+              <label for="lastName">Last Name</label>
+              <input type="name"></input>
+            </p>
+            <input type="radio"></input>
+            <input type="radio"></input>
+            <input type="radio"></input>
+            <input type="radio"></input>
+            <p>
+              <label for="color"> Enter a Color</label>
+              <input type="color" id="color" />
+            </p>
+            <button>Submit</button>
+
+          </form>
+          <form action="https://www.google.com/search">
+            <input type="text" name="q"></input>
+            <button>search</button>
+          </form>
           <span><p>TES<sub>T</sub></p></span>
         </div>
+        <section>
+          <p>skdjhfklsdjflskdjflsdkjfsdklfjsdlkfj</p>
+          <aside><ps>aside test</ps></aside>
+        </section>
+        <p>
+          <label for="Entre"></label>
+          <select>
+            <option value>--Please Choose an Option</option>
+            <option value="dog">Dog</option>
+            <option value="dog">Cat</option>
+            <option value="dog">Bird</option>
+            <option value="dog">Chimp</option>
+            <option value="dog">Bruh</option>
+          </select>
+        </p>
+        <p>
+          <label for="Entre"></label>
+          <select name="meal" is="meal">
+            <option value="dog">Dog</option>
+            <option value="dog">Cat</option>
+            <option value="dog">Bird</option>
+            <option value="dog">Chimp</option>
+            <option value="dog">Bruh</option>
+          </select>
+        </p>
+        <p>
+          <label for="Cheese">Amount of Cheese</label>
+          <input type="range"></input>
+        </p>
+        <nav>
+          <ul>
+            <li><a href="home">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="blah.html">Blah</a></li>
+          </ul>
+        </nav>
         <h3>
           <ui>
             <li>Hello</li>
@@ -32,12 +99,60 @@ function App() {
             <li>Hello</li>
           </ui>
         </h3>
-      </header>
+        <h1>Birds</h1>
+        <table>
+          <tr>
+            <th>TESST</th>
+            <th>TESST</th>
+            <th>TESST</th>
+            <th>TESST</th>
+            <th>TESST</th>
+            <th>TESST</th>
+            <th>TESST</th>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+          <tr>
+            <td>BSDSSDFSDF</td>
+            <td>ffff</td>
+            <td>9387</td>
+          </tr>
+        </table>
+      </header >
       <First />
       <Second />
       <Third />
       <Fourth />
-    </div>
+      <footer><p>This is my footer</p></footer>
+    </div >
   );
 }
 
